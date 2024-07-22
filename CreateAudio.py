e3c3a7db2dfc249f9254436bce963fd359847685
Isaac Vaughn <isaac1vaughn@gmail.com>
2024-07-22 17:30:13 -0500

    GetRandomMP4 returns a random vid from the Backgrounds folder; Create video uses the random mp4 + audio file to create the final product

--- CreateAudio.py
+++ CreateAudio.py
@@ -1,7 +1,37 @@
 from gtts import gTTS
-
+from moviepy.editor import *
 import os
+import random
 
-def CreateAudioFile(my_text: str, title: str):
-    obj = myobj = gTTS(text=my_text, lang='en', tld='com.au')
-    myobj.save(f"{title}.mp3")
+def CreateVideo(audio_file: AudioFileClip):
+    mp4_path = GetRandomMP4()
+    background = VideoFileClip(mp4_path)
+    start = random.randint(0, int(background.duration) - int(audio_file.duration))
+    end = start + audio_file.duration
+
+    new_vid = background.subclip(start, end)
+
+    new_vid = new_vid.set_audio(audio_file)
+
+    new_vid.write_videofile("my_new_video.mp4", codec="libx264")
+    background.close()
+    new_vid.close()
+    audio_file.close()
+
+    
+    
+
+def GetRandomMP4():
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+    relative_path = os.path.join('Backgrounds', 'GTAV.mp4')
+
+    return os.path.join(base_dir, relative_path)
+
+
+def CreateAudioFile(my_text: str, title: str):  
+    obj = gTTS(text=my_text, lang='en', tld='com.au')
+    obj.save(f"{title}.mp3")
+
+    return AudioFileClip(f"{title}.mp3")
+
+
--- CreateBackground.py
+++
@@ -1,2 +0,0 @@
-from moviepy import *
-
